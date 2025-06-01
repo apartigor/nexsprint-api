a447c9c55cc4b2de676a6dd36c73791066b520fb
apartigor <contact.jojigor@gmail.com>
2025-06-01 20:48:22 -0300

    refactor: :zap: Performance na pagina pós login melhorada com imagens de baixa resolução mas qualidade alta

--- nexsprintAPI/Program.cs
+++ nexsprintAPI/Program.cs
@@ -40,13 +40,13 @@ using (var scope = app.Services.CreateScope())
     {
         var querysql = @"
         INSERT INTO modulos (Nome, PDF_Url, Descricao, TotalPaginas, Capa_Url) VALUES
-        ('Módulo 1: Introdução ao Agile Modeling', 'pdfs/am1.pdf', 'Fundamentos do Agile Modeling para suportar a colaboração ágil', 4, 'capas/am1.jpg'),
-        ('Módulo 2: Práticas Essenciais', 'pdfs/am2.pdf', 'As práticas fundamentais para uma modelagem ágil eficiente', 4, 'capas/am2.jpg'),
-        ('Módulo 3: Ferramentas e Técnicas', 'pdfs/am3.pdf', 'Ferramentas práticas para facilitar sessões de modelagem.', 4, 'capas/am3.jpg'),
-        ('Módulo 4: Colaboração e Comunicação', 'pdfs/am4.pdf', 'Como a modelagem impulsiona a colaboração ágil', 4, 'capas/am4.jpg'),
+        ('Módulo 1: Introdução ao Agile Modeling', 'pdfs/am1.pdf', 'Fundamentos do Agile Modeling para suportar a colaboração ágil', 4, 'capas/am1.webp'),
+        ('Módulo 2: Práticas Essenciais', 'pdfs/am2.pdf', 'As práticas fundamentais para uma modelagem ágil eficiente', 4, 'capas/am2.webp'),
+        ('Módulo 3: Ferramentas e Técnicas', 'pdfs/am3.pdf', 'Ferramentas práticas para facilitar sessões de modelagem.', 4, 'capas/am3.webp'),
+        ('Módulo 4: Colaboração e Comunicação', 'pdfs/am4.pdf', 'Como a modelagem impulsiona a colaboração ágil', 4, 'capas/am4.webp'),
         ('Módulo 5: Casos Reais', 'pdfs/am5.pdf', 'Exemplos práticos de aplicação do Agile Modeling.', 4, 'capas/am5.jpg'),
-        ('Módulo 6: Introdução ao Design Sprint', 'pdfs/ds1.pdf', 'Primeiro contato com Design Sprint e suas aplicações', 4, 'capas/ds1.jpg'),
-        ('Módulo 7: Estrutura dos 5 dias', 'pdfs/ds2.pdf', 'Como é estruturado um Design Sprint completo.', 4, 'capas/ds2.jpg'),
+        ('Módulo 6: Introdução ao Design Sprint', 'pdfs/ds1.pdf', 'Primeiro contato com Design Sprint e suas aplicações', 4, 'capas/ds1.webp'),
+        ('Módulo 7: Estrutura dos 5 dias', 'pdfs/ds2.pdf', 'Como é estruturado um Design Sprint completo.', 4, 'capas/ds2.webp'),
         ('Módulo 8: Técnicas e Ferramentas', 'pdfs/ds3.pdf', 'Ferramentas práticas para potencializar o Sprint.', 4, 'capas/ds3.jpg'),
         ('Módulo 9: Facilitando um Sprint', 'pdfs/ds4.pdf', 'Como liderar um Sprint de maneira eficiente.', 4, 'capas/ds4.jpg'),
         ('Módulo 10: Estudos de Caso', 'pdfs/ds5.pdf', 'Aplicações reais e resultados do Design Sprint.', 4, 'capas/ds5.jpg');
